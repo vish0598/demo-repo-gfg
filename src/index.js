13f1912bc2318a4ef80c3cd8fb3636acf565b495
vish0598 <vishaljethwani11@gmail.com>
2021-10-28 18:52:52 +0530

    included scss file support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
-
+//import "./style/index.scss"
+import './styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
